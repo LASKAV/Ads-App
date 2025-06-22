957934c010fe0cf881ac0fa5fbed78f4e285a5af
LASKAV <mksidoops@gmail.com>
2025-06-22 11:24:02 +0200

    Initial Commit

---
+++ MobileTechnicalChallenge/MobileTechnicalChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileTechnicalChallengeApp.swift
+//  MobileTechnicalChallenge
+//
+//  Created by MYKHAIL LASKAVYI on 22/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct MobileTechnicalChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
