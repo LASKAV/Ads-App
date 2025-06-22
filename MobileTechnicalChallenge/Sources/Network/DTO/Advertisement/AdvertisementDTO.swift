cfb9f455c4d0cfcafa0209a285af72f442b8932d
LASKAV <mksidoops@gmail.com>
2025-06-22 17:19:55 +0200

    Refactor AdvertisementDTO to conform to Codable and remove unused properties

--- MobileTechnicalChallenge/Sources/Network/DTO/Advertisement/AdvertisementDTO.swift
+++ MobileTechnicalChallenge/Sources/Network/DTO/Advertisement/AdvertisementDTO.swift
@@ -1,16 +1,9 @@
 import Foundation
 
 
-struct AdvertisementDTO: Decodable {
+struct AdvertisementDTO: Codable {
     let description: String
-    let id: String
-    let url: String
-    let adType: String
-    let location: String
-    let type: String
-    let price: PriceDTO
+    let price: PriceDTO?
     let image: ImageDTO
-    let score: Double
-    let version: String
-    let favourite: FavouriteDTO
+    let location: String
 }
