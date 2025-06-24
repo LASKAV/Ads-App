7d6a41a8f577789e1acaa9311194121450a985a8
LASKAV <mksidoops@gmail.com>
2025-06-24 17:35:10 +0200

    refactor: Clean up AdvertisementMapper.swift by removing unnecessary comments and improving code readability

--- MobileTechnicalChallenge/Features/Advertisement/Mappers/AdvertisementMapper.swift
+++ MobileTechnicalChallenge/Features/Advertisement/Mappers/AdvertisementMapper.swift
@@ -1,7 +1,17 @@
-//
-//  m.swift
-//  MobileTechnicalChallenge
-//
-//  Created by MYKHAIL LASKAVYI on 24/06/2025.
-//
+import Foundation
 
+
+extension Advertisement {
+    init(dto: AdvertisementDTO) {
+        let basePhotoURL = "https://images.finncdn.no/dynamic/480x360c/"
+        self.photo = basePhotoURL + (dto.image?.url ?? "")
+
+        let priceValue = Int(dto.price?.value ?? 0)
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        self.price = formatter.string(from: NSNumber(value: priceValue)) ?? "\(priceValue)"
+
+        self.location = dto.location
+        self.title = dto.description
+    }
+}
