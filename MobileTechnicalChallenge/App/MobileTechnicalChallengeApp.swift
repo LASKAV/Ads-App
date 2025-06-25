59ba473c02cf1bd75e1ac5e0799bf40d1b7ef9be
LASKAV <mksidoops@gmail.com>
2025-06-26 00:59:51 +0200

    feat: Update main app structure to use AdsListView with FavoritesStore environment

--- MobileTechnicalChallenge/App/MobileTechnicalChallengeApp.swift
+++ MobileTechnicalChallenge/App/MobileTechnicalChallengeApp.swift
@@ -2,9 +2,19 @@ import SwiftUI
 
 @main
 struct MobileTechnicalChallengeApp: App {
+    
+    private var favoritesStore = FavoritesStore()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AdsListView()
+                .environment(favoritesStore)
         }
     }
 }
+
+#Preview {
+    AdsListView()
+        .environment(FavoritesStore())
+}
+
